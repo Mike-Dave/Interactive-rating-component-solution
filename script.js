733ec684c7ba61882c8c3405c3269a610ed06e63
Michael Chukwu <macdevis26@gmail.com>
2023-05-02 22:38:03 +0200

    Updated the JS code
      I applied my knowledge of event delegation

--- script.js
+++ script.js
@@ -3,27 +3,39 @@
 const submitBtn = document.querySelector(".submit");
 const firstContainer = document.querySelector(".first-container");
 const secondContainer = document.querySelector(".second-container");
-const numberSelector = document.querySelectorAll(".item");
+const numbers = document.querySelectorAll(".item");
 const selectText = document.querySelector(".selection-text");
+const numberContainer = document.querySelector(".number-container");
 
-/* 
-A variable to check when a number has been clicked.
-To stop the submit button from submitting at first click (on page reload)
-*/
+// A variable to check when a number has been clicked.
 let clicked;
 
 // Submit button to listen foe clicks
-submitBtn.addEventListener("click", function () {
+submitBtn.addEventListener("click", function (e) {
+  // To stop the submit button from submitting at first click (on page reload)
+  e.preventDefault();
   if (clicked) {
     secondContainer.classList.remove("hidden");
     firstContainer.classList.add("hidden");
+  } else {
+    alert("Please select a number!");
   }
 });
 
-// To listen for clicks on  any of the numbers
-numberSelector.forEach((el) =>
-  el.addEventListener("click", function () {
-    clicked = el.textContent;
-    selectText.textContent = `You selected ${el.textContent} out of 5`;
-  })
-);
+// To listen for clicks on any of the numbers
+
+numberContainer.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("item")) {
+    // Removing the active color from all the numbers
+
+    numbers.forEach((el) => {
+      el.classList.remove("active");
+      clicked = e.target.id;
+      selectText.textContent = `You selected ${clicked} out of 5`;
+    });
+
+    // Adding the active color to the clicked number
+    e.target.classList.add("active");
+  }
+});
